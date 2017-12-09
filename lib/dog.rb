10719804b6fba85cc50f563d8f6a37090f009300
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-09 15:27:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,7 +28,9 @@ class Dog
     self
   end
   def self.create(hash)
-    dog = Dog.new(@name,@breed)
+    name = "name"
+    breed = "breed"
+    dog = Dog.new(name,breed)
     binding.pry
     hash.each do |key,value|
       dog.send("#{key}=",value)
