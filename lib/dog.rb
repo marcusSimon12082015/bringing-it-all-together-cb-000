0d701ba73101813d7393f9ca9680619ff70dd363
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-09 13:25:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :id,:name,:breed
-   
+
   def initialize(name,breed,id=nil)
-      @id = id 
-      @name = name 
+      @id = id
+      @name = name
       @breed = breed
-  end 
+  end
 end 
