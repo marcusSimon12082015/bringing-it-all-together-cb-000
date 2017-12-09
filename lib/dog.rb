317ccd863ea8a37e152ca4bd2a8ae6198144b820
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-09 13:47:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,13 +19,13 @@ class Dog
   end
   def save
     sql = <<-SQL
-      INSERt INTO dogs(name,breed) VALUES(?,?)
+      INSERT INTO dogs(name,breed) VALUES(?,?)
     SQL
     DB[:conn].execute(sql,self.name,self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     self
   end
   def self.create(hash)
-    
-  end 
+
+  end
 end
