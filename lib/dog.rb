2247d6a46741c157cc3a5338b9478c196154e253
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-09 13:32:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,10 @@ class Dog
     @name = name
     @breed = breed
   end
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs(id INTEGER PRIMARY KEY,name INTEGER,breed TEXT)
+    SQL 
+    DB[:conn].execute(sql)
+  end 
 end
